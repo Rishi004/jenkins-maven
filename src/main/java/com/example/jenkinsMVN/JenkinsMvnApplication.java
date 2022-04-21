f72696dd4d37895d8424484c6ef030651141284b
Rishi004 <rishireiko@gmail.com>
2022-04-21 09:23:05 +0530

    FIrst Commit Maven

---
+++ src/main/java/com/example/jenkinsMVN/JenkinsMvnApplication.java
@@ -0,0 +1,21 @@
+package com.example.jenkinsMVN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsMvnApplication {
+	
+	@GetMapping("/")
+	public String hello () {
+		return "Hello!";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsMvnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkinsMVN/JenkinsMvnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsMVN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsMvnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
